24a8577f54c2b581190a3075c4c5d316d8f635ea
Danny <nguyenduongpa@gmail.com>
2022-07-24 16:33:23 -0700

    Fixed the issue of player typing in the wrong input the second time and shutting down the program. Now the try block will catch as many exceptions as necessary before moving on to the next step.

--- guessing_game.py
+++ guessing_game.py
@@ -8,13 +8,17 @@ def start_game():
 
     attempts = 0
     random_number = random.randint(1, 10)
-    
+
     if scores:
         print("The current highest score is {}. Try to beat it!".format(min(scores)))
-    try:
-        guess = int(input("Please guess a number between 1 and 10!  "))
-    except ValueError:
-        guess = int(input("Please use a VALID number between 1 and 10!  "))
+
+    while True:
+        try:
+            guess = int(input("Please guess a number between 1 and 10!  "))
+            break
+        except ValueError:
+            print("Opps! That was not valid! Please try again!")
+            
     if guess == 0:
         guess = int(input("Oops! Zero isn't a valid choice. We won't count this towards your attempts. Please choose a number between 1 and 10!  "))
     if guess > 10:
