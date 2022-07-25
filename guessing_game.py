43cca40cf0350592fa773946d4f646ab538c5158
Danny <nguyenduongpa@gmail.com>
2022-07-24 17:18:11 -0700

    Wrapped the entire block of code in a try block in order to catch more ValueErrors. Now, even if the first couple guesses are valid, if the player proceeds to input an invalid guess, it will catch the error and the program would continue smoothly. PLUS it will still ONLY counts the valid guesses as the player's attempts.

--- guessing_game.py
+++ guessing_game.py
@@ -20,30 +20,33 @@ def start_game():
             print("Opps! That was not valid! Please try again!")
 
     while True:
-        if guess == 0:
-            guess = int(input("Oops! Zero isn't a valid choice. We won't count this towards your attempts. Please choose a number between 1 and 10!  "))
-        elif guess > 10:
-            guess = int(input("Oops! That number is higher than 10. We won't count this towards your attempts. Please choose a number between 1 and 10!  "))
-        elif guess == random_number:
-            print("You got it {}! Great job!".format(player_name))
-            attempts += 1
-            print("It took you {} attempts to guess the correct answer!".format(attempts))
-            scores.append(attempts)
-            if attempts == min(scores):
-                print("Congrats! You have the current highest score of {} attempts!".format(attempts))
-            play_again = input("Would you like to play again? Y/N  ")
-            if play_again.lower() == "y":
-                start_game()
-            elif play_again.lower() == "n":
-                print("Have a wonderful day and thank you for playing, {}!".format(player_name))
-                quit()
-        elif guess < random_number:
-            guess = int(input("It's lower than the solution! Try again!  "))
-            attempts += 1
-            continue
-        elif guess > random_number:
-            guess = int(input("It's higher than the solution! Try again!  "))
-            attempts += 1
-            continue
+        try:
+            if guess == 0:
+                guess = int(input("Oops! Zero isn't a valid choice. We won't count this towards your attempts. Please choose a number between 1 and 10!  "))
+            elif guess > 10:
+                guess = int(input("Oops! That number is higher than 10. We won't count this towards your attempts. Please choose a number between 1 and 10!  "))
+            elif guess == random_number:
+                print("You got it {}! Great job!".format(player_name))
+                attempts += 1
+                print("It took you {} attempts to guess the correct answer!".format(attempts))
+                scores.append(attempts)
+                if attempts == min(scores):
+                    print("Congrats! You have the current highest score of {} attempts!".format(attempts))
+                play_again = input("Would you like to play again? Y/N  ")
+                if play_again.lower() == "y":
+                    start_game()
+                elif play_again.lower() == "n":
+                    print("Have a wonderful day and thank you for playing, {}!".format(player_name))
+                    quit()
+            elif guess < random_number:
+                guess = int(input("It's lower than the solution! Try again!  "))
+                attempts += 1
+                continue
+            elif guess > random_number:
+                guess = int(input("It's higher than the solution! Try again!  "))
+                attempts += 1
+                continue
+        except ValueError:
+            print("Oops! That was not valid! Please try again!")        
 
 start_game()
